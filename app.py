fa12a211923e57dc0e4b03e3e7853c7f003420b5
Tareq Judeh <tareq.judeh@outlook.com>
2021-02-13 13:46:32 +0200

    Fixed all HTML pages links to correctly direct users to the right targeted page. Implemented Dynamic HTML pages route, instead of hard coding rendering each page in app.py.

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def my_home():
+    """Main webpage"""
     return render_template("index.html")
 
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    """Accessing HTML pages dynamically:"""
+    return render_template(page_name)
 
 
 if __name__ == '__main__':
