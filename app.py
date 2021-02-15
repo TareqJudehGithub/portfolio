bf4a36c865a469c000939bd71681db05eab07e09
Tareq Judeh <tareq.judeh@outlook.com>
2021-02-15 17:55:15 +0200

    Form submittion now writes and saves data in a .CSV files instead of a .txt file.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from time import sleep
+import csv
 
 app = Flask(__name__)
 
@@ -16,18 +16,57 @@ def html_page(page_name):
     return render_template(page_name)
 
 
+def write_to_file(data):
+    with open("database.txt", mode="a") as database:
+        name = data["username"]
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        database.write(
+            f"\n"
+            f"Name:    {name}\n"
+            f"Email:   {email}\n"
+            f"Subject: {subject}\n"
+            f"Message: {message}\n"
+        )
+
+
+def write_to_csv(data):
+    with open("database.csv",  newline="", mode="a") as database2:
+        name = data["username"]
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+
+        csv_writer = csv.writer(
+            database2,
+            delimiter=",",  # separator type .
+            quotechar="'",  # quote type for special characters.
+            quoting=csv.QUOTE_MINIMAL
+        )
+
+        # contain the data we need and save it:
+        csv_writer.writerow(
+            [
+                name,
+                email,
+                subject,
+                message
+            ]
+        )
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     """Flask requests"""
     if request.method == "POST":
         data = request.form.to_dict()
         name = request.form["username"]
-        print(data)
-        print(name)
+
+        write_to_csv(data)
+
         # return redirect("/thankyou.html")
-
         return render_template("thankyou.html", name=name)
-
 
     else:
         return "Something went wrong."
@@ -35,3 +74,4 @@ def submit_form():
 
 if __name__ == '__main__':
     app.run()
+
